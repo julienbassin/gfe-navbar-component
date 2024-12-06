eedb0d9db427e34c6a023d2da0da632f196189ed
julien.bassin <julien.bassin@avanade.com>
2024-12-06 20:21:15 +0100

    finishing first layout for mobile

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+document.addEventListener("DOMContentLoaded", () =>{
+    const menuButton = document.getElementById("mobile-menu-button");
+    const closeButton = document.getElementById("mobile-close-menu-button");
+    const slideOutMenu = document.getElementById("slideout-menu");
+
+    menuButton?.addEventListener("click", () => {
+        slideOutMenu.style.display  = "block";
+    });
+
+    // close the menu
+
+    closeButton?.addEventListener("click", () => {
+        slideOutMenu.style.display = "none";
+    });
+})
